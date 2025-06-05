4bad4e81f1ddad547b0e0d0682ae42e73997ff16
RayqRohit <rrajput1172@gmail.com>
2025-06-05 10:06:35 +0530

    Modified : Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,19 +34,67 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 
 /*==================== SHOW SCROLL UP ====================*/
 
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        const navLink = document.querySelector('.nav-link[href*=' + sectionId + ']');
+
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add('active-link');
+            } else {
+                navLink.classList.remove('active-link');
+            }
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+
+const sr = ScrollReveal({
+    // origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+});
+
+
+sr.reveal(`.home-header, .section-title`, { delay: 600 })
+sr.reveal(` .home-footer`, { delay: 700 })
+sr.reveal(`.home-img`, { delay: 900, origin: 'top' })
+sr.reveal(`.sponsor-img , .products-cards , .footer-logo, .footer-content, .footer-copy`, { interval: 100, origin: 'top' })
+sr.reveal(`.specs-data , .discount-animate`, { origin: 'left', interval: 100 })
+sr.reveal(`.specs-img, .discount-img`, { origin: 'right' })
+sr.reveal(`.case-img`, { origin: 'top' })
+sr.reveal(`.case-data`, { origin: 'bottom' })
